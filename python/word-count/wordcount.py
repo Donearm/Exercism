b6e670b6e099a81893ef7071f9364741bdc87c79
Gianluca Fiore <forod.g@gmail.com>
2014-11-07 09:17:43 +0100

    Better variable naming in wordcount.py

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -3,17 +3,17 @@
 
 def word_count(phrase):
     # Strip out newlines
-    p = phrase.strip('\n')
+    clean_phrase = phrase.strip('\n')
     # Split at each whitespace
-    s = p.split()
+    splitted_phrase = clean_phrase.split()
     
     # Dictionary to contain the word count
-    d = {}
-    for i in s:
-        if i in d:
-            # if the word, "i", is already in d, add 1
-            d[i] += 1
+    finaldict = {}
+    for w in splitted_phrase:
+        if w in finaldict:
+            # if the word, "i", is already in finaldict, add 1
+            finaldict[w] += 1
         else:
-            d[i] = 1
+            finaldict[w] = 1
 
-    return d
+    return finaldict
