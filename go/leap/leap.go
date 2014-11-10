48ddb810580ab8c3fc78e74c9f9d806a8d6af3b9
Gianluca Fiore <forod.g@gmail.com>
2014-11-10 09:10:41 +0100

    Second, shorter and faster, iteration of Leap (Go)

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,10 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	// If it's divisible by 400, it's also divisible by 4 and 100 so this goes 
-	// first
-	if year % 400 == 0 {
-		return true
-	} else if year % 100 == 0 {
-		// Divisible by 100? Then not leap year
-		return false
-	} else if year % 4 == 0 {
-		return true
+	if year % 4 == 0 {
+		if year % 100 != 0 || year % 400 == 0 {
+			return true
+		}
 	}
-
-	// required return statement at end of function. False by default
 	return false
 }
