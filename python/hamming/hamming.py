2ae5adb64bebdab86a9f8a76a85c17c2a5a89c0d
Gianluca Fiore <forod.g@gmail.com>
2015-06-20 15:06:29 +0200

    c variable wasn't being used in hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -4,7 +4,7 @@
 def distance(seq1, seq2):
     result = 0
     for i, c in enumerate(seq1):
-        if seq1[i] is not seq2[i]:
+        if c is not seq2[i]:
             result += 1
 
     return result
