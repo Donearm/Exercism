650756c105538a7233734684451a9cf98883b23e
Gianluca Fiore <forod.g@gmail.com>
2015-06-29 13:44:59 +0200

    Solution to Triangle (Go) exercise

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -3,18 +3,28 @@ package triangle
 type Kind string
 
 const (
-  Equ = "equilateral"
-  Iso = "isosceles"
-  Sca = "scalene"
-  NaT = "not a triangle"
+	Equ = "equilateral"
+	Iso = "isosceles"
+	Sca = "scalene"
+	NaT = "not a triangle"
 )
 
 func KindFromSides(a, b, c float64) Kind {
 	switch {
-		case (a + b) <= c || (a + c) <= b || (b + c) <= a: return NaT
-		case (a == b) && (b == c): return Equ
-		case (a == b) || (b == c) || (a == c): return Iso
-		case (a + b) > c || (a + c) > b || (b + c) > a: return Sca
-		default: return NaT
+	// Negative sides, NaN and Inf
+	case !(a > 0 && b > 0 && c > 0):
+		return NaT
+	// Check for triangle inequality
+	case (a+b) <= c || (a+c) <= b || (b+c) <= a:
+		return NaT
+	// All sides equal, equilateral
+	case a == b && b == c:
+		return Equ
+	// 2 sides equal, isosceles
+	case a == b || b == c || a == c:
+		return Iso
+	// Otherwise it must be a scalene
+	default:
+		return Sca
 	}
 }
